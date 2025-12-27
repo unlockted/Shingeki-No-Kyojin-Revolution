b1de0dda14b976c07127eb31f506ac4f166f7609
unlockted <ch3esyxd@gmail.com>
2025-12-27 14:07:15 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
 if game.PlaceId == 13379208636 then
   print(game.Players.LocalPlayer.Name)
-
-
-
-
-
-
+  loadstring(game:HttpGet("https://arcanecheats.xyz/api/matcha/uilib"))()
+repeat wait() until Arcane
+end
