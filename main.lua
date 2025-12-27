0293f257f0a233dfb0a72129cf2e3b1f2a5379cf
unlockted <ch3esyxd@gmail.com>
2025-12-27 14:11:19 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 13379208636 then
 	print(game.Players.LocalPlayer.Name)
 	local Arcane = loadstring(game:HttpGet("https://arcanecheats.xyz/api/matcha/uilib"))()
 	repeat wait() until Arcane
+	
+	local Window = Arcane:CreateWindow("AOTR by unlockted", Vector2.new(650, 450), "RosePine")
 end
